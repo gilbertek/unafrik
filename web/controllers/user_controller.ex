6e5ab33d50a35f04ed2daa7c43a54b2ac803b8f0
Gilbert F. Sewovoe-Ekoue <gsewovoe-ekoue@teladoc.com>
2017-02-07 16:19:55 -0500

    fx

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -14,13 +14,13 @@ defmodule Unafrik.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
 
     case Repo.insert(changeset) do
       {:ok, _user} ->
         conn
         |> put_flash(:info, "User created successfully.")
-        |> redirect(to: user_path(conn, :index))
+        |> redirect(to: page_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
