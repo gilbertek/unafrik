0d103877e483e6d59cdcb4d71de4e947bb6da94c
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-02-16 21:58:09 -0500

    updatesc

--- web/static/js/app/app.js
+++ web/static/js/app/app.js
@@ -1,5 +1,2 @@
-// Required to make Webpack compile css assets properly
-// require('./../css/app.sass');
-
-// import 'phoenix';
-// import 'phoenix_html';
+import 'phoenix';
+import 'phoenix_html';
