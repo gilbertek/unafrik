4e445422f0354a5c973dea7082eb3919d0c1a03b
Gilbert F. Sewovoe-Ekoue <gilberts55@hotmail.com>
2017-02-16 21:56:30 -0500

    cleanups

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,6 @@ var merge = require("webpack-merge");
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-var bourbon = require('bourbon').includePaths
-var neat = require('bourbon-neat').includePaths
-
-var env = process.env.MIX_ENV || 'dev';
-var isProduction = (env === 'prod')
-var TARGET = process.env.npm_lifecycle_event;
 
 var common = {
   context: __dirname,
@@ -19,24 +13,21 @@ var common = {
   target: 'async-node',
 
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
         exclude: [/node_modules/, /semantic/, /uploads/],
-        use: [
-          {
+        use: [{
             loader: "babel-loader",
-            options: { presets: ["es2015"] },
-          }
-        ],
+            options: { presets: ["es2015"] }
+          }],
       },
       {
         test: /\.hbs$/,
-        use: [{ loader: "handlebars-loader" }]
+        use: [{ loader: "handlebars-loader"}]
       },
       {
         test: /\.less$/,
-        use: [{ loader: "less-loader" }]
+        use: [{loader: "less-loader"}]
       },
       // {
       //   test: /\.css$/,
@@ -48,7 +39,15 @@ var common = {
       //     },
       //   ]
       // },
-
+      {
+        test: [/\.sass$/, /\.css$/, /\.scss$/],
+        loader: ExtractTextPlugin.extract(
+          {
+            fallback: "style-loader",
+            loader: "css-loader!sass-loader"
+          }
+        )
+      },
       // {
       //   test: /\.(sass|scss)$/,
       //   use: [
@@ -57,13 +56,6 @@ var common = {
       //     'sass-loader',
       //   ]
       // },
-      {
-        test: [/\.sass$/, /\.css$/, /\.scss$/],
-        loader: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          loader: "css-loader!sass-loader"
-        })
-      },
       {
         test: /\.(png|jpg|gif|svg)$/,
         use: [
@@ -75,7 +67,6 @@ var common = {
           }
         ]
       },
-
       {
         test: /\.(eot|ttf|woff|woff2)(\?\S*)?$/,
         loader: "file-loader",
@@ -93,11 +84,14 @@ var common = {
       output: {comments: false},
       sourceMap: true
     }),
-
-    new webpack.LoaderOptionsPlugin({ minimize: true }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    }),
 
     new webpack.DefinePlugin({
-      "process.env": { NODE_ENV: JSON.stringify("production") }
+      "process.env": {
+          NODE_ENV: JSON.stringify("production")
+      }
     }),
   ]
 };
@@ -111,13 +105,19 @@ module.exports = [
     },
     output: {
       path: "./priv/static",
-      filename: "js/[name].[chunkhash].js"
+      filename: "js/[name].js"
     },
     resolve: {
       modules: ["node_modules", __dirname + "/web/static/app"],
+      moduleExtensions: ['.js', '.jsx'],
       alias: {
         "jquery": path.resolve(__dirname, "node_modules/jquery/dist/jquery.js"),
-        "modernizr": path.resolve(__dirname, "node_modules/modernizr/src/Modernizr.js"),
+        'jQuery': path.join(__dirname,
+                            'node_modules',
+                            'jquery',
+                            'dist',
+                            'jquery.js'),
+        "normalize": path.resolve(__dirname, "node_modules/normalize.css/normalize.css"),
       }
     },
     plugins: [
@@ -141,75 +141,55 @@ module.exports = [
       filename: "js/[name].[chunkhash].js"
     },
     resolve: {
-      modules: ["node_modules", __dirname + "/web/static/app"]
+      modules: [
+        "node_modules",
+        __dirname + "/web/static/app"
+      ],
     },
     plugins: [
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery"
+      }),
       new CopyWebpackPlugin([{ from: "./web/static/assets"}]),
-      new ExtractTextPlugin("css/admin.css")
+      new ExtractTextPlugin({filename: "css/[name].css", allChunks: true})
     ]
   }),
 
   // Admin Style with semantic-ui-css Entry Point
-  merge(common, {
-    entry: {
-      semantic: [
-        // "./web/static/css/admin-semantic/calendar.css",
-        // "./web/static/js/admin-semantic/calendar.js",
-        "./web/static/css/admin-semantic/admin_semantic.scss",
-        "./web/static/js/admin-semantic/admin_semantic.js"]
-    },
-    output: {
-      path: "./priv/static",
-      filename: "js/[name].[chunkhash].js"
-    },
-    resolve: {
-      alias: {
-        "jquery": path.resolve(__dirname, "node_modules/jquery/dist/jquery.js"),
-        "modernizr": path.resolve(__dirname, "node_modules/modernizr/src/Modernizr.js"),
-        'semantic-ui': path.resolve(__dirname,
-                                  'node_modules/semantic-ui-css/semantic.js'),
-        'semantic-ui-calendar': path.resolve(
-                          __dirname,
-                          'node_modules/semantic-ui-calendar/dist/calendar.js')
-      }
-    },
-    plugins: [
-      new webpack.ProvidePlugin({$: "jquery", jQuery: "jquery"}),
-      new ExtractTextPlugin("css/admin_semantic.css")
-    ]
-  }),
-
-  // Admin Style with materialize css Entry Point
-  merge(common, {
-    entry: {
-      materialize: [
-        "./web/static/css/admin-materialize/admin_materialize.scss",
-        "./web/static/js/admin-materialize/admin_materialize.js"]
-    },
-
-    output: {
-      path: "./priv/static",
-      filename: "js/[name].[chunkhash].js"
-    },
-
-    resolve: {
-      modules: ["node_modules", path.resolve(__dirname + "/web/static/js")],
-      alias: {
-        "jquery": path.resolve(__dirname, "node_modules/jquery/dist/jquery.js"),
-        "modernizr": path.resolve(__dirname, "node_modules/modernizr/src/Modernizr.js"),
-        'materialize': path.join(__dirname,
-                                  'node_modules',
-                                  'materialize-css',
-                                  'sass',
-                                  'materialize.scss'),
-      }
-    },
-
-    plugins: [
-      new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery" }),
-      new ExtractTextPlugin("css/admin_semantic.css")
-    ]
-  }),
+  // merge(common, {
+  //   entry: {
+  //     dashboard: [
+  //       "./web/static/css/dashboard/calendar.css",
+  //       "./web/static/js/dashboard/calendar.js",
+  //       "./web/static/css/dashboard/dashboard.scss",
+  //       "./web/static/js/dashboard/dashboard.js"]
+  //   },
+  //   output: {
+  //     path: "./priv/static",
+  //     filename: "js/[name].js"
+  //   },
+  //   resolve: {
+  //     modules: ["node_modules", __dirname + "/web/static/app"],
+  //     alias: {
+  //       "jquery": path.resolve(__dirname, "node_modules/jquery/dist/jquery.js"),
+  //       'semantic-ui': path.join(__dirname,
+  //                                 'node_modules',
+  //                                 'semantic-ui-css',
+  //                                 'semantic.js'),
+  //       'semantic-ui-calendar': path.join( __dirname,
+  //                                           'node_modules',
+  //                                           'semantic-ui-calendar',
+  //                                           'dist',
+  //                                           'calendar.js')
+  //     }
+  //   },
+  //   plugins: [
+  //     new CopyWebpackPlugin([{from: "./web/static/assets"}]),
+  //     new webpack.ProvidePlugin({$: "jquery", jQuery: "jquery"}),
+  //     new ExtractTextPlugin({filename: "css/[name].css", allChunks: true})
+  //   ]
+  // }),
 
   // Emails Style Entry Point
   merge(common, {
@@ -221,6 +201,6 @@ module.exports = [
     resolve: {
       modules: ["node_modules", __dirname + "/web/static/app"]
     },
-    plugins: [new ExtractTextPlugin("css/email.css")]
+    plugins: [new ExtractTextPlugin({ filename: "css/email.css" })]
   })
 ];
